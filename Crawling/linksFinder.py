63a15e2278c56aec22c8c6aea552854d8a73ac95
alifthi <alifathi8008@gmail.com>
2023-04-26 17:35:07 +0430

    crawler: now this script can find all urls in that linkes to the mainUrl directly or indirectly

--- Crawling/linksFinder.py
+++ Crawling/linksFinder.py
@@ -26,10 +26,9 @@ def extractLinks(url):
 mainUrl = 'https://fa.wikipedia.org'
 url = 'https://fa.wikipedia.org/wiki/%D8%B5%D9%81%D8%AD%D9%87%D9%94_%D8%A7%D8%B5%D9%84%DB%8C'
 links = extractLinks(url)
-allLinks = links
 for link in links:
     url = mainUrl + link
     pageLinks = extractLinks(url)
     if pageLinks == None:
         continue
-    allLinks.extend(link)
+    links.extend(link)
