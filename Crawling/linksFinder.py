c47929288511e948132cf9d4cc6cfbaacaa295f5
alifthi <alifathi8008@gmail.com>
2023-04-26 16:57:13 +0430

    crawler: create a for loop to collect all links in extracted links

--- Crawling/linksFinder.py
+++ Crawling/linksFinder.py
@@ -9,6 +9,27 @@ def getResponse(url):
 def findLinks(htmlDoc):
     soup = bs(htmlDoc)
     return soup.find_all('a')
-
+def extractLinks(url):
+    response = getResponse(url)
+    if response == None:
+        return None
+    link = findLinks(response.text)
+    links = []
+    for li in link:
+        try:
+            href = str(li.get('href'))
+        except:
+            continue
+        if ('#' not in href) and not ('extiw' == li.get('class')) and '.org' not in href:
+            links.append(href)
+    return links
+mainUrl = 'https://fa.wikipedia.org'
 url = 'https://fa.wikipedia.org/wiki/%D8%B5%D9%81%D8%AD%D9%87%D9%94_%D8%A7%D8%B5%D9%84%DB%8C'
-extractLinks(url)
+links = extractLinks(url)
+allLinks = links
+for link in links:
+    url = mainUrl + link
+    pageLinks = extractLinks(url)
+    if pageLinks == None:
+        continue
+    allLinks.extend(link)
