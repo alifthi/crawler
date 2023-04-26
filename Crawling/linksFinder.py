104fe3e2c244691f3e2599731584fd2d11a5ca3a
alifthi <alifathi8008@gmail.com>
2023-04-26 15:33:37 +0430

    crawling: extractLinks function developed to extact all links in a url and store in a page

--- Crawling/linksFinder.py
+++ Crawling/linksFinder.py
@@ -9,9 +9,12 @@ def getResponse(url):
 def findLinks(htmlDoc):
     soup = bs(htmlDoc)
     return soup.find_all('a')
-
+def extractLinks(url):
+    response = getResponse(url)
+    link = findLinks(response.text)
+    links = [li.get('href').encode('utf-8') for li in link if ('#' not in li.get('href')) and not ('extiw' == li.get('class')) and '.org' not in li.get('href')]
+    for li in links:
+        print(li)           
+mainUrl = 'https://fa.wikipedia.org'
 url = 'https://fa.wikipedia.org/wiki/%D8%B5%D9%81%D8%AD%D9%87%D9%94_%D8%A7%D8%B5%D9%84%DB%8C'
-mainUrl = 'https://fa.wikipedia.org/'
-response = getResponse(url)
-link = findLinks(response.text)
-links = [li.get('href') for li in link]
+extractLinks(url)
