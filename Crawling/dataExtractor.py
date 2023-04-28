2cb294bd4a802ea993c559c6c7de76f5f08b067e
alifthi <alifathi8008@gmail.com>
2023-04-28 15:52:13 +0430

    crawler: loadLinks method developed to load the links that saved in a text file.

--- Crawling/dataExtractor.py
+++ Crawling/dataExtractor.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup as bs
+
 class dataExtractor:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,mainUrl) -> None:
+        self.mainUrl = mainUrl
     def start(self):
         pass
-    def loadLinks(self):
-        pass    
+    def loadLinks(self,urlAddr):
+        with open(urlAddr,'r') as file:
+            self.links = file.read()
+        return self.links
--- Crawling/main.py
+++ Crawling/main.py
@@ -1,20 +1,20 @@
 import sys
 
 if __name__ == '__main__':
-    
+    mainUrl = 'https://fa.wikipedia.org'
     url = '/wiki/%D8%B5%D9%81%D8%AD%D9%87%D9%94_%D8%A7%D8%B5%D9%84%DB%8C'
-    switch = 'extractUrl' # sys.argv[1]
+    switch = sys.argv[1]
     links = [url]
     if switch == 'extractUrl':
         from linksFinder import linksExtractor
-        lEx = linksExtractor('https://fa.wikipedia.org')
+        lEx = linksExtractor(mainUrl)
         links = lEx.start(numLinks=10)
-        lEx.saveLinks(links=links,addr='.\\links.txt')
-    elif switch == 'extractCorpus':
+        lEx.saveLinks(links=links,addr='./links.txt')
+    elif switch == 'extractData':
         from dataExtractor import dataExtractor as extractor
-        extractor = extractor()
-        links = extractor.loadLinks()
-        for link in links:
-            extractor.start(link)
+        extractor = extractor(mainUrl)
+        links = extractor.loadLinks('../links/links.txt')
+        # for link in links:
+        #     extractor.start(link)
     elif switch == 'extractOneLink':
         pass
